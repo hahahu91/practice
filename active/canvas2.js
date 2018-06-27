4aaab760e0e8d3d4775f9c8bacdaed4e7d8970b6
Roman Morozov <hahahu91@gmail.com>
2018-06-27 18:59:54 +0300

    ready animate piechart

--- active/canvas2.js
+++ active/canvas2.js
@@ -9,7 +9,7 @@ ctx.font = '10px Arial';
 const ANIMATION_DURATION = 1000;
 const DX = 100;
 const DY = 100;
-const ANIMATE_DY = 200;
+const ANIMATE_DY = CANVAS_HEIGHT/9;
 const ITEM_WIDTH = 200;
 const ITEM_HEIGHT = 200;
 const COLORS =["red", "blue", "green", "darkgrey", "brown", "grey", "yellow", "pink"];
@@ -18,7 +18,9 @@ var model = {
         this.items.push({
             id: "item" + this.items.length,
             value: val,
+            dY: 0,
             active: false,
+            start: 0,
             color: COLORS[this.items.length%COLORS.length],
         });
         model.total += val;
@@ -43,19 +45,31 @@ model.addItem(100);
 model.addItem(100);
 model.addItem(100);
 
-function getItemRect(item, model) {
+function getItemRect1(item, model) {
     let radiant = (Math.PI * 2) * (item.value / model.total)
     return {
         rad: radiant,
-        cx: CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 2.8*Math.cos((model.start + model.start + radiant)/2),
-        cy: CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 2.8*Math.sin((model.start + model.start + radiant)/2),
-        x: !item.active ? CANVAS_WIDTH / 2 : CANVAS_WIDTH / 2 + 20,
-        y: !item.active ? CANVAS_HEIGHT / 2 : CANVAS_HEIGHT / 2 + 20,
+        cx: !item.active ? CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 2.8*Math.cos((model.start + model.start + radiant)/2) : CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + CANVAS_HEIGHT / 9)*Math.cos((model.start + model.start + radiant)/2),
+        cy: !item.active ? CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 2.8*Math.sin((model.start + model.start + radiant)/2) : CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + CANVAS_HEIGHT / 9)*Math.sin((model.start + model.start + radiant)/2),
+        x: !item.active ? CANVAS_WIDTH / 2 : CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 9*Math.cos((item.start + item.start + radiant)/2),
+        y: !item.active ? CANVAS_HEIGHT / 2 : CANVAS_WIDTH / 2 + CANVAS_HEIGHT / 9*Math.sin((item.start + item.start + radiant)/2),
+    };
+}
+function getItemRect(item, model) {
+    let radiant = (Math.PI * 2) * (item.value / model.total);
+    return {
+        rad: radiant,
+        cx: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item.dY)*Math.cos((model.start + model.start + radiant)/2),
+        cy: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item.dY)*Math.sin((model.start + model.start + radiant)/2),
+        x: CANVAS_WIDTH / 2 + item.dY*Math.cos((item.start + item.start + radiant)/2),
+        y: CANVAS_WIDTH / 2 + item.dY*Math.sin((item.start + item.start + radiant)/2),
     };
 }
 function  drawOneElem(ctx, objData, model, index) {
     const itemRect = getItemRect(objData, model);
     (index == model.items.length-1 && index%COLORS.length == 0) ? ctx.fillStyle = COLORS[1] :ctx.fillStyle = objData.color;
+    objData.start = model.start;
+    objData.rect = itemRect;
     ctx.beginPath();
         ctx.arc(itemRect.x, itemRect.y, CANVAS_HEIGHT / 3, model.start, model.start+itemRect.rad, false);
         ctx.lineTo(itemRect.x, itemRect.y);
@@ -63,11 +77,11 @@ function  drawOneElem(ctx, objData, model, index) {
     const text = Math.round(objData.value / model.total*100) + '%';
     writeProcents(text, itemRect.cx, itemRect.cy);
     //добавляем в модель данные
-        //objData.start = model.start;
-       // objData.proc = text;
-    //
     model.start += itemRect.rad;
     model.start = model.start%(Math.PI*2);
+    //добавляем в модель данные
+        objData.finish = model.start;
+    //console.log(model);
 }
 
 function printChart(ctx, model){
@@ -76,7 +90,7 @@ function printChart(ctx, model){
         drawOneElem(ctx, item, model, index);
     });
 }
-printChart(ctx, model);
+
 
 function writeProcents(text, x, y){
     ctx.textBaseline="middle";
@@ -96,9 +110,44 @@ function startRecalculation(event) {
     model.curent = inRad(mousePos.deg);
     model.oldStart = model.start%(Math.PI*2);
     let requestId = window.requestAnimationFrame(animate);
-    console.log('x', mousePos.x);
-    console.log('y', mousePos.y);
-    console.log('deg', mousePos.deg);
+    let hypo = Math.sqrt(mousePos.x*mousePos.x+mousePos.y*mousePos.y);
+    if (hypo <= (CANVAS_HEIGHT / 3 + CANVAS_HEIGHT / 9)) {
+        activateEl(model, inRad(mousePos.deg), hypo, mousePos);
+    }
+}
+
+function activElementCheck(item, model, mousePos) {
+    const rect = getItemRect(item, model);
+    const activeX = mousePos.x-rect.x+CANVAS_WIDTH/2;
+    const activeY = mousePos.y-rect.y+CANVAS_HEIGHT/2;
+    const hypo = Math.sqrt(activeX*activeX+activeY*activeY);
+    const Deg = inRad(arctg360(activeX, activeY));
+    if (Deg >= item.start && Deg < item.finish && hypo <= CANVAS_HEIGHT/3) {
+            processElementClick(model.items, item);
+        }
+    else if (item.start > item.finish) {
+            if ((Deg >= item.start || Deg < item.finish)&&hypo <= CANVAS_HEIGHT/3)
+                processElementClick(model.items, item);
+    }
+}
+
+function activateEl(modelEl, deg, hypotenuse, mousePos){
+    modelEl.items.forEach((item) => {
+        if (deg >= item.start && deg < item.finish ) {
+                if (item.active)
+                    activElementCheck(item, model, mousePos);
+                else if (hypotenuse <= CANVAS_HEIGHT / 3)
+                    processElementClick(model.items, item);
+            }
+        else if (item.start > item.finish) {
+            if (deg >= item.start || deg < item.finish) {
+                if (item.active)
+                    activElementCheck(item, model, mousePos);
+                else if (hypotenuse <= CANVAS_HEIGHT / 3)
+                    processElementClick(model.items, item);
+            }
+        }
+    });
 }
 
 function recalculation(event) {
@@ -112,13 +161,33 @@ function recalculation(event) {
     }
 }
 
-function animate() {
-    if (model.prevCur != model.degCur) {
+function animate1() {
+    if (model.prevCur != model.degCur || model.isRecalculation == true) {
         printChart(ctx, model);
     };
     requestAnimationFrame(animate);
 }
-
+printChart(ctx, model);
+function animate() {
+    let start = performance.now();
+    model.items.forEach((item, index) => {
+        if (item.duration > 0) {
+            let timeFraction = (start - item.activeTime ) / ANIMATION_DURATION;
+            if (timeFraction > 1) timeFraction = 1;
+            item.duration = ANIMATION_DURATION * (1 - item.baseProgress)-timeFraction*ANIMATION_DURATION;
+            const progress = timeFraction*ANIMATION_DURATION/ANIMATION_DURATION + item.baseProgress;
+            const prog = (ANIMATION_DURATION-item.duration)/(ANIMATION_DURATION * (1-item.baseProgress));
+            item.dY = item.active 
+                ? 0 + progress * ANIMATE_DY
+                : ANIMATE_DY - progress * ANIMATE_DY;
+            printChart(ctx, model);
+            let time = performance.now();
+        }
+        else item.duration = 0;
+    });
+    requestAnimationFrame(animate);
+}
+animate();
 
 function stopRecalculation() {
     model.isRecalculation = false;
@@ -160,13 +229,15 @@ function initHandlers(canvasEl, model) {
             if (mousePos.x >= rect.x && mousePos.x <= rect.x+rect.width && 
                mousePos.y >= rect.y && mousePos.y <= rect.y+rect.height)
                 {
+                    let rect = getItemRect(item, model);
                     processElementClick(model.items, item);
-                    console.log('!');
+                    console.log('atan' , rect.atan);
+                    
                 }
         });
     }, false);
 }
-initHandlers(canvas, model);
+//initHandlers(canvas, model);
 
 function processElementClick(items, clickedItem) {
     items.forEach((item) => {
@@ -178,6 +249,15 @@ function processElementClick(items, clickedItem) {
         }
     });
 }
+function activate(item, isActive){
+    item.baseProgress = item.active ? (1 - item.dY / ANIMATE_DY) : (item.dY / ANIMATE_DY);
+    item.active = isActive;
+    item.duration = ANIMATION_DURATION * (1 - item.baseProgress);
+    console.log('baseProg', item.baseProgress);
+    console.log('duration', item.duration);
+    console.log('dY', item.dY);
+    item.activeTime = performance.now();
+}
 
 
 function inRad(degrees){
@@ -188,10 +268,3 @@ function inDeg(radians){
     return radians*180/Math.PI;
 }
 
-function getMousePos(canvas, even) {
-    let rect = canvas.getBoundingClientRect();
-    return {
-        x: even.clientX - rect.left,
-        y: even.clientY - rect.top
-    };
-}
