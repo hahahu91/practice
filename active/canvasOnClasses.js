abc2e1ae1be0986f565ad88b99ba2d17731de948
Roman Morozov <hahahu91@gmail.com>
2018-06-29 15:53:19 +0300

    canvas on classes

--- active/canvasOnClasses.js
+++ active/canvasOnClasses.js
@@ -9,6 +9,7 @@ ctx.font = '10px Arial';
 const ANIMATION_DURATION = 1000;
 const ANIMATE_DY = CANVAS_HEIGHT/9;
 const COLORS =["red", "blue", "green", "darkgrey", "brown", "grey", "yellow", "pink"];
+
 var model = {
     addItem: function(val) {
         this.items.push({
@@ -30,55 +31,110 @@ var model = {
     degCur: 0,
     curent: (Math.PI*3)/2,
 }
-animate();
-model.addItem(10);
-model.addItem(30);
-model.addItem(50);
-model.addItem(60);
-model.addItem(100);
-model.addItem(100);
-model.addItem(100);
-model.addItem(100);
-model.addItem(100);
+class Item {
+    constructor(value, color, index) {
+        this.id = "Item" + index;
+        this.index = index;
+        this.value = value;
+        this._isActive = false;
+        this._color = color;
+        this._animation = null;
+        this.start = null;
+        model.total += value;
+    }
+    setColor(c) {
+        this._color = c;
+    }
+    getColor() {
+        return this._color;
+    }
+    draw(canvasContext, rotationDegree, totalValue) {
+        const itemRect = getItemRect(this, rotationDegree, totalValue);
+        canvasContext.fillStyle = this._color;
+       // console.log(this);
+        canvasContext.beginPath();
+            canvasContext.arc(itemRect.x, itemRect.y, CANVAS_HEIGHT / 3, rotationDegree, rotationDegree+itemRect.rad, false);
+            canvasContext.lineTo(itemRect.x, itemRect.y);
+        canvasContext.fill();
+        const text = Math.round(this.value /totalValue*100) + '%';
+        writeProcents(canvasContext, text, itemRect.cx, itemRect.cy);
+        rotationDegree += itemRect.rad;
+        rotationDegree = rotationDegree%(Math.PI*2);
+    }
 
-function getItemRect(item, model) {
-    let radiant = (Math.PI * 2) * (item.value / model.total);
+    animation() {
+        return this._animation;
+    }
+
+    activate() {
+        this._isActive = true;
+        this._animation = createAnimation();
+    }
+
+    deactivate() {
+        this._isActive = false;
+        this._animation = createAnimation();
+    }
+}
+
+class PieChart {
+
+    constructor() {
+        this._items = [];
+        this._colors = ["red", "blue", "green"];//, "darkgrey", "brown", "grey", "yellow", "pink"];
+        this._rotationDegree = (Math.PI*3)/2;
+        this.total = 0;
+        this.isRecalculation = false;
+    }
+
+    addItem(value) {
+        this._items.push(new Item(value, this._colors[this._items.length%this._colors.length], this._items.length));
+        this.total += value;
+    }
+    
+    rotateAt(degree) {
+        this._rotationDegree = degree;
+    }
+
+    draw(canvasContext) {
+        canvasContext.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        this._items.forEach((items, index) => {
+            const itemRect = getItemRect(items, this._rotationDegree, this.total);
+            let color = items.getColor();
+            if (color == this._colors[0] && index == this._items.length-1) { 
+                items.setColor(this._colors[1]);
+            }
+            items.draw(canvasContext, this._rotationDegree, this.total);
+            this._rotationDegree += itemRect.rad;
+            this._rotationDegree = this._rotationDegree%(Math.PI*2);
+        });
+    }
+}
+const pieChart = new PieChart();
+pieChart.addItem(120);
+pieChart.addItem(110);
+pieChart.addItem(20);
+pieChart.addItem(101);
+pieChart.addItem(120);
+pieChart.addItem(180);
+
+pieChart.addItem(180);
+pieChart.draw(ctx);
+
+//animate();
+
+function getItemRect(item, start, total) {
+    let radiant = (Math.PI * 2) * (item.value / total);
     return {
         rad: radiant,
-        cx: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item.dY)*Math.cos((model.start + model.start + radiant)/2),
-        cy: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item.dY)*Math.sin((model.start + model.start + radiant)/2),
-        x: CANVAS_WIDTH / 2 + item.dY*Math.cos((item.start + item.start + radiant)/2),
-        y: CANVAS_WIDTH / 2 + item.dY*Math.sin((item.start + item.start + radiant)/2),
+        cx: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item._animation)*Math.cos((start + start + radiant)/2),
+        cy: CANVAS_WIDTH / 2 + (CANVAS_HEIGHT / 2.8 + item._animation)*Math.sin((start + start + radiant)/2),
+        x: CANVAS_WIDTH / 2 + item._animation*Math.cos((start + start + radiant)/2),
+        y: CANVAS_WIDTH / 2 + item._animation*Math.sin((start + start + radiant)/2),
     };
 }
-function  drawOneElem(ctx, objData, model, index) {
-    const itemRect = getItemRect(objData, model);
-    (index == model.items.length-1 && index%COLORS.length == 0) ? ctx.fillStyle = COLORS[1] :ctx.fillStyle = objData.color;
-    objData.start = model.start;
-    objData.rect = itemRect;
-    ctx.beginPath();
-        ctx.arc(itemRect.x, itemRect.y, CANVAS_HEIGHT / 3, model.start, model.start+itemRect.rad, false);
-        ctx.lineTo(itemRect.x, itemRect.y);
-    ctx.fill();
-    const text = Math.round(objData.value / model.total*100) + '%';
-    writeProcents(text, itemRect.cx, itemRect.cy);
-    //добавляем в модель данные
-    model.start += itemRect.rad;
-    model.start = model.start%(Math.PI*2);
-    //добавляем в модель данные
-        objData.finish = model.start;
-    //console.log(model);
-}
 
-function printChart(ctx, model){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        model.items.forEach((item, index) => {
-        drawOneElem(ctx, item, model, index);
-    });
-}
-
-
-function writeProcents(text, x, y){
+function writeProcents(ctx, text, x, y){
     ctx.textBaseline="middle";
     ctx.textAlign="center";
     ctx.fillStyle = 'black';
@@ -86,13 +142,14 @@ function writeProcents(text, x, y){
 }
 
 canvas.addEventListener("mousedown", startRecalculation, false);
-canvas.addEventListener("click", activateElement, false);
+//canvas.addEventListener("click", activateElement, false);
 document.addEventListener("mouseup", stopRecalculation, false);
 document.addEventListener("mousemove", recalculation, false);
 
 function startRecalculation(event) {
-    model.isRecalculation = true;
+    pieChart.isRecalculation = true;
     let mousePos = mousecoordinates(canvas, event);
+    //pieChart.rotateAt = 
     model.degCur = inDeg(model.curent);
     model.curent = inRad(mousePos.deg);
     model.oldStart = model.start%(Math.PI*2);
@@ -142,8 +199,9 @@ function activateEl(modelEl, deg, hypotenuse, mousePos){
 }
 
 function recalculation(event) {
-    if (model.isRecalculation == true) {
+    if (pieChart.isRecalculation == true) {
         let mousePos = mousecoordinates(canvas, event);
+        
         model.degCur = inDeg(model.curent);
         let cur = mousePos.deg - model.degCur;
         if (cur < 0) {cur += 360};
@@ -151,7 +209,7 @@ function recalculation(event) {
     }
 }
 
-printChart(ctx, model);
+//printChart(ctx, model);
 function animate() {
     let start = performance.now();
     model.items.forEach((item, index) => {
@@ -164,12 +222,14 @@ function animate() {
             item.dY = item.active 
                 ? 0 + progress * ANIMATE_DY
                 : ANIMATE_DY - progress * ANIMATE_DY;
-            printChart(ctx, model);
+            
+            pieChart.draw(ctx);
+            //printChart(ctx, model);
             let time = performance.now();
         }
         else item.duration = 0;
     });
-    if (model.prevCur != model.degCur) printChart(ctx, model);
+    if (model.prevCur != model.degCur) pieChart.draw(ctx);
     requestAnimationFrame(animate);
 }
 animate();
